362a09030d20fcd87963c9fa2b82c4a00e0b43ad
rajarshisadhya <rajarshisadhya@gmail.com>
2026-05-02 20:45:07 +0530

    Add CSRF trusted origins for Render

--- Restaurant_Project/settings.py
+++ Restaurant_Project/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['django-restaurant-project-5a2k.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://django-restaurant-project-5a2k.onrender.com']
+
 
 # Application definition
 
